92a44c787bb779f191d658cb2401b871ba25fff0
Tony Wu <tony_wu@usiglobal.com>
2020-12-23 14:06:54 +0800

    Initial Project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+   
+    class Program
+    {
+        private static readonly uint FIRST_DISPLAY = 1;
+
+        static int Main(string[] args)
+        {
+            if (args[0] == "90")
+            {
+              
+                //First display is 1.
+                Display.Rotate(FIRST_DISPLAY, Display.Orientations.DEGREES_CW_90);
+            }
+            else if (args[0] == "0")
+            {
+                //First display is 1.
+                Display.Rotate(FIRST_DISPLAY, Display.Orientations.DEGREES_CW_0);
+            }
+
+            else if (args[0] == "270")
+            {
+                //First display is 1.
+                Display.Rotate(FIRST_DISPLAY, Display.Orientations.DEGREES_CW_270);
+            }
+
+            else if (args[0] == "180")
+            {
+                //First display is 1.
+                Display.Rotate(FIRST_DISPLAY, Display.Orientations.DEGREES_CW_180);
+            }
+
+            return 0;
+            
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+For Sherpa Demo 
+1.ConsoleApp1 Rotation Windows Screen
+2.user_hid Receive and Send Data to USB Device
